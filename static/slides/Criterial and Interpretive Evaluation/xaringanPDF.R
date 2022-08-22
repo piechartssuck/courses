aebe40f6a5ae5e188ca846a7d26bba189ea21ea7
Abhik Roy <olgratefuldead@gmail.com>
2022-08-22 08:36:43 -0400

    edp618

--- static/slides/Criterial and Interpretive Evaluation/xaringanPDF.R
+++
@@ -1,6 +0,0 @@
-# Set the working directory as source ----
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-remotes::install_github("rstudio/chromote")
-source("https://git.io/xaringan2pdf")
-xaringan_to_pdf("Criterial-and-Interpretive-Eval.html")
